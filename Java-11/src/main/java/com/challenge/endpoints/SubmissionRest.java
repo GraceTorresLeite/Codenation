e0e8fcb5ddee9dd760baaa9a816eee9c3fd2666b
GraceTorresLeite <37751047+GraceTorresLeite@users.noreply.github.com>
2020-03-10 15:25:34 -0300

    Add files via upload

---
+++ Java-11/src/main/java/com/challenge/endpoints/SubmissionRest.java
@@ -0,0 +1,35 @@
+package com.challenge.endpoints;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.challenge.entity.Submission;
+import com.challenge.service.impl.SubmissionService;
+
+@RestController
+@RequestMapping("/submission")
+public class SubmissionRest {
+	
+	private SubmissionService submissionService;
+
+    @Autowired
+    public SubmissionRest(SubmissionService submissionService) {
+        this.submissionService = submissionService;
+    }
+
+    @GetMapping()
+    public List<Submission> findByChallengeIdAndAccelerationId(
+            @RequestParam Long challengeId,
+            @RequestParam Long accelerationId) {
+
+        List<Submission> byChallengeIdAndAccelerationId = submissionService.findByChallengeIdAndAccelerationId(challengeId, accelerationId);
+
+        return byChallengeIdAndAccelerationId;
+    }
+
+}
