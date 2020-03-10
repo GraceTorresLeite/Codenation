68c7aa31ecc1ee0eee05a62d5a53739196223a6b
GraceTorresLeite <37751047+GraceTorresLeite@users.noreply.github.com>
2020-03-10 15:16:23 -0300

    Add files via upload

---
+++ java-1/src/main/java/br/com/codenation/IdentificatorUtilizadoException.java
@@ -0,0 +1,13 @@
+package br.com.codenation;
+
+public class IdentificatorUtilizadoException extends Exception {
+	private static final long serialVersionUID = 1L;
+	 
+	public IdentificatorUtilizadoException() {
+		
+	}
+	public IdentificatorUtilizadoException (String msg, Throwable cause) {
+		super(msg, cause);
+	}
+
+}
